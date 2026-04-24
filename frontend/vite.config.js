b6062cafcd0e7cf39159d4114742db4495edd3a2
hmtstc-creator <hmtstc@gmail.com>
2026-04-24 13:49:11 +0300

    viteduzenleme

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      }
-    }
-  },
   build: {
     outDir: 'dist',
     sourcemap: false,
   }
 })
+``
